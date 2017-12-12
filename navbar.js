fb7d8a0eccf71a1b4ffa3a55571533fa02e748c7
Joey Bahary <jbahary2@illinois.edu>
2017-12-11 23:20:19 -0600

    Lel

--- navbar.js
+++ navbar.js
@@ -3,4 +3,5 @@ document.getElementById("navMenu").innerHTML =
   '<li class="default"><a href="index.html">Game</a></li>' +
   '<li class="default"><a href="settings.html">Settings</a></li>' +
   '<li class="default"><a href="howtoplay.html">The Point</a></li>' +
+  '<li class="default"><a href="ball-game/index.html">Ball Game</a></li>'
 '</ul>';
